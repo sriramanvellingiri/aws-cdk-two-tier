ff3c1548e8ce0bccb67454b267e43a45beb7c7fa
sriraman <sriramanvellingiri@gmail.com>
2024-05-10 17:50:30 +0530

    fix: initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # aws-cdk-two-tier
 this CDK implement react and node jS app using AWS
+
+# architecture
---
+++ cdk-demo/bin/cdk-demo.ts
@@ -0,0 +1,17 @@
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { FargateDemoStack } from "../lib/fargate";
+import { CloudfrontDemoStack } from "../lib/cloudfront";
+
+const app = new cdk.App();
+
+// Cloudfront stack
+// new CloudfrontDemoStack(app, "CloudfrontDemoStack", {
+//   stage: "prod",
+//   env: { account: "869228520926", region: "us-east-1" },
+// });
+
+// Fargate stack
+new FargateDemoStack(app, "FargateDemoStack", {
+  env: { account: "869228520926", region: "us-east-1" },
+});
